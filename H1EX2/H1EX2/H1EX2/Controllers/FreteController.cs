f0e1a784b119b23aa3a24833efe219f148398127
sct7555 <151705649+sct7555@users.noreply.github.com>
2024-08-18 17:34:31 -0300

    Add files via upload

---
+++ H1EX2/H1EX2/H1EX2/Controllers/FreteController.cs
@@ -0,0 +1,44 @@
+﻿using H1EX2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace H1EX2.Controllers {
+    [ApiController]
+    [Route("API/Frete")]
+    public class FreteController : ControllerBase {
+
+        private readonly Dictionary<string, decimal> tarifasPorEstado = new Dictionary<string, decimal>
+     {
+            { "SP", 50.00m },
+            { "RJ", 60.00m },
+            { "MG", 55.00m },
+            { "OUTROS", 70.00m }
+        };
+
+        private const decimal taxaPorCm3 = 0.01m;
+
+        [HttpPost]
+        [HttpPost("calcular-frete")]
+        public IActionResult CalcularFrete(Frete _frete) {
+            
+            float volume = (float)(_frete.Altura * _frete.Largura * _frete.Comprimento);
+
+        
+            string uf = _frete.UF.ToUpper();
+            decimal tarifaEstado = tarifasPorEstado.ContainsKey(uf) ? tarifasPorEstado[uf] : tarifasPorEstado["OUTROS"];
+
+          
+            decimal valorFrete = (decimal)volume * taxaPorCm3 + tarifaEstado;
+
+            var resultado = new {
+                NomeProduto = _frete.Nome,
+                Peso = _frete.Peso,
+                Volume = volume,
+                UF = uf,
+                TarifaEstado = tarifaEstado,
+                ValorFrete = Math.Round(valorFrete, 2)
+            };
+
+            return Ok(resultado);
+        }
+    }
+}
---
+++ H1EX2/H1EX2/H1EX2/Models/Frete.cs
@@ -0,0 +1,11 @@
+﻿namespace H1EX2.Models {
+    public class Frete {
+        public string Nome { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+        public double Largura { get; set; }
+        public double Comprimento { get; set; }
+        public string UF { get; set; }
+    }
+}
+
